1f143197d0c2816dc54a39a0e5fdd97bfecbbe1a
marcin <marcinbho@gmail.com>
2022-01-15 09:15:55 +0100

    Add file

---
+++ ćwiczenie 1.py
@@ -0,0 +1,16 @@
+# Koniec gry
+print("Program'Koniec gry'")
+print("Taki sam", "Kominikat", "jak przedtem")
+print("tylko",
+       "nieco",
+       "większy")
+print("Oto", end=" ")
+print("on...")
+print("""K
+o
+n
+i
+e
+c
+ GRY""")
+input("\n\nAby zakończyć program, naciśnij klawisz ESC.")
