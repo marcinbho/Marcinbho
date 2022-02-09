4383a1d8aa2b48b96957a0e47e4cb9535d119256
marcin <marcinbho@gmail.com>
2022-02-09 18:27:36 +0100

    Add file

--- Cwiczenia 02.02.py
+++ Cwiczenia 02.02.py
@@ -37,31 +37,101 @@
 #             account.info()
 #             account.wplac(500)
 #             account.wyplac(1000)
+#
+# class A:
+#     """Rodzic pierwszy"""
+#
+#     def __init__(self):
+#         super().__init__()
+#         self.a = "A"
+#
+#     def fa(self):
+#         print("a:", self.a)
+#
+#
+# class B:
+#     """Rodzic drugi"""
+#
+#     def __init__(self):
+#         super().__init__()
+#         self.b = "B"
+#
+#     def fb(self):
+#         print("b:", self.b)
+#
+#
+# class Pochodna(B, A):
+#     """Dziecko"""
+#
+#     def __init__(self):
+#         super().__init__()
+#         print(A.__doc__)
+#         print(B.__doc__)
+#         print(Pochodna.__doc__)
+# import math
+# class Figura:
+#     def obwod(self):  # L
+#         """Obliczanie obwodu."""
+#         raise NotImplementedError
+#
+#     def pole(self):  # S/P
+#         """Obliczanie pola powierzchni."""
+#         raise NotImplementedError
 
-class A:
-    """Rodzic pierwszy"""
-
-    def __init__(self):
-        super().__init__()
-        self.a = "A"
-
-    def fa(self):
-        print("a:", self.a)
-
-
-class B:
-    """Rodzic drugi"""
-
-    def __init__(self):
-        super().__init__()
-        self.b = "B"
-
-    def fb(self):
-        print("b:", self.b)
+    #     # print(math.pi)
+    # class kolo(Figura):
+    #     def __init__(self, r):
+    #         self.r = r
+    #         def obwod(self):
+    #         return self.r*2 * math.pi
+    #
+    #         def pole(self):
+    #         return self.r ** 2 * math.pi
+    # k=kolo ( 5 )
+    # print(k.pole())
 
 
-class Pochodna(B, A):
-    """Dziecko"""
 
-    def __init__(self):
-        super().__init__()
+
+
+
+# class trujkat(Figura):
+#     def __init__(self, a):
+#         self.a = a
+#     def obwod(self):
+#         return self.a * 3
+#
+#     def pole(self):
+#         return self.a ** 2 * 3 ** (1/2)/4
+# f2 = trujkat (5)
+# print(a.obwod())
+# print(a.pole())
+
+
+# class prostokat(Figura):
+#     def __init__(self, a, b):
+#         self.a = a
+#         self.b = b
+#     def obwod(self):
+#         return 2 * (self.a + self.b)
+#
+#     def pole(self):
+#         return self.a * self.b
+# f3 = prostokat(2.4)
+# print(f3 . pole)
+# print(f3 . obwod)
+
+#Kwadrat
+
+# class kwadrat(Figura):
+#     def __init__(self,a):
+#         self.a = a
+#
+#     def obwod(self):
+#         return 4 * self.a
+#
+#     def pole(self):
+#         return self.a ** 2
+# f3 = kwadrat(4)
+# print(f3 . pole)
+# print(f3 . obwod)
--- ćwiczenie 1.py
+++ ćwiczenie 1.py
@@ -1,56 +1,53 @@
-# Użyj finally do zamykania obiektów i czyszczenia zasobów.
-# Spróbuj otworzyć i zapisać (podpowiedź: write) w pliku, którego nie można zapisać.
-# Zapewnij, aby program mógł kontynuować bez pozostawiania otwartego obiektu pliku.
 
-# # Otwórz plik
-# fo = open("text.txt", "r")
-# print("Nazwa pliku: ", fo.name)
-#
-# line = fo.readline()
-# print("Czytaj linię: >" + line + "<")
-#
-# # Ponownie ustaw wskaźnik na początek
-# fo.seek(0, 0)  # fo.seek(0)
-# line = fo.readline()
-# print("Czytaj linię: >" + line + "<")
-#
-# # Zamknij otwarty plik
-# fo.close()
 
-# # Otwórz plik
-# fo = open("text.txt", "r")
-# print("Nazwa pliku: ", fo.name)
-#
-# # Uzyskaj aktualną pozycję pliku.
-# pos = fo.tell()
-# print("Aktualna pozycja: " + str(pos))
-#
-# line = fo.readline()
-# print("Czytaj linię: >" + line + "<")
-#
-# # Uzyskaj aktualną pozycję pliku.
-# pos = fo.tell()
-# print("Aktualna pozycja: " + str(pos))
-#
-# # # Zamknij otwarty plik
-# # fo.close()
-#
-# import os
-# os.system("dir")
-# pitagoras = lambda a, b:  ((a * a) + (b * b)) ** 0.5
-# print(pitagoras(3, 4))
+__import__(math)
 
-# l_sum = lambda x, y: x + y
-# # print(c_sum(3, 4))
-# print(l_sum(3, 4))
+#
+# class Figura:
+#     def obwod(self):  # L
+#         """Obliczanie obwodu."""
+#         raise NotImplementedError
+#
+#     def pole(self):  # S/P
+#         """Obliczanie pola powierzchni."""
+#         raise NotImplementedError
+# class trapez(Figura):
+#     def __init__(self, a, b, h):
+#         self.a, self.b, self.h = a, b, h
+#     def obwod(self):
+#         return self.a + self.b + self.h + (self.h ** 2 )
 
-# temperatury = [
-#     37.6, 35.8, 37.6, 33.4, 34.1, 37.1, 35.9, 34.1, 37.1, 40.5, 38.5, 37.6,
-#     35.8, 34.5, 36.4, 38.3, 37.5, 37.7, 34.0, 35.3, 35.7, 38.9, 34.8, 34.1,
-#     39.6, 35.4, 34.7, 37.6, 38.4, 36.4, 39.8, 39.1, 37.1, 35.6, 36.8, 37.6,
-#     36.7, 40.0, 38.0, 34.1, 35.5, 38.5, 36.1, 32.6, 32.9, 34.5, 41.0, 38.3,
-#     33.7, 38.7, 36.9, 36.2, 33.7, 38.3, 35.3, 38.3, 40.1, 39.3, 38.2, 37.6,
-#     39.1, 37.1, 34.4, 38.7, 35.8, 38.2, 38.2, 33.1, 37.8, 36.5, 37.6, 37.4,
-#     34.3, 37.7, 36.0, 37.5, 37.6, 36.5, 31.3, 37.7, 40.3, 39.5, 35.7, 38.1,
-#     34.7, 36.5, 34.3, 38.0, 37.0, 38.5, 39.4, 37.6, 41.7, 40.0, 38.4, 38.9,
-#     34.2, 40.2, 34.3, 35.3
+# Utwórz obiekt klasy Bus, która dziedziczy wszystkie zmienne i metody klasy Vehicle i wyświetli je.
+
+# class Vehicle:
+#
+#     def __init__(self, name, max_speed, mileage):
+#         self.name = name
+#         self.max_speed = max_speed
+#         self.mileage = mileage
+#
+# class Vehicle:
+#     def __init__(self, name, max_speed, mileage):
+#         self.name = name
+#         self.max_speed = max_speed
+#         self.mileage = mileage
+#
+#     def seating_capacity(self, capacity):
+#         return f"Liczba miejsc siedzących w {self.name} to {capacity} pasażerów"
+# class bus(Vehicle):
+#
+#     def seating_capacity(self, capacity):
+#         return 4 * self.a
+
+# class Vehicle:
+#
+#     def __init__(self, name, max_speed, mileage):
+#         self.name = name
+#         self.max_speed = max_speed
+#         self.mileage = mileage
+#
+# class Bus(Vehicle):
+#     pass
+#
+# class Car(Vehicle):
+#     pass
